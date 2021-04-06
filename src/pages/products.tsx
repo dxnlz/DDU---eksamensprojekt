b2f689cc982108d82aa44ec28df5065486924e74
Mathias Gredal <mathiasgredal@icloud.com>
2021-04-06 13:55:23 +0000

    Began on flexbox layout

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -6,17 +6,25 @@ import styles from '../styles/Products.module.scss';
 export default class ProductPage extends Component {
     render() {
         return (
-            <>
+            <div style={{flex: "1 1 auto", flexDirection: "column", justifyItems: "stretch", display:"flex"}}>
                 <div className={styles.searchText}>
                     <span >Search results for </span>
                     <i>"Lego brik"</i>
                 </div>
-                <Button variant="contained" color="secondary">
-                    Secondary
-                </Button>
 
-                <CellGrid />
-            </>
+                <main style={{display: "flex", flex: 1,alignItems: "stretch", justifyItems: "stretch", marginBottom: "1rem"}}>
+                    <div style={{display: "flex", flex: 1, backgroundColor: "blue", alignItems: "center", justifyContent: "center", marginRight: "1rem", borderRadius: "4px"}}>
+                        Left
+                    </div>
+
+                    <div  style={{display: "flex", flex: 3, backgroundColor: "red", alignItems: "center", justifyContent: "center", borderRadius: "4px"}}>
+                        Right
+                    </div>
+                </main>
+
+                
+                
+            </div>
         )
     }
 }
