63d5024bde88ea2481a26aab4accb6cda1d7a6cc
Emil Biørn <emil620k@zbc.dk>
2021-04-14 18:48:38 +0000

    Started on admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,5 +1,8 @@
-import * as React from 'react';
-import { Component } from 'react';
+import { Button, FormControl, FormHelperText, Input, InputLabel, Slider, TextField, Typography } from "@material-ui/core";
+import React, { Component } from "react";
+import CellGrid from "../components/CellGrid"
+import styles from '../styles/Products.module.scss';
+
 
 export interface AdminPageProps {
     
@@ -13,9 +16,40 @@ class AdminPage extends React.Component<AdminPageProps, AdminPageState> {
     constructor(props: AdminPageProps) {
         super(props);
     }
-    render() { 
-        return ( <div>admin</div> );
+    render() {
+        return (
+            <>
+                <div className={styles.searchText}>
+                    <span >Admin side: </span>
+                    <i>"Red Lego brik"</i>
+                </div>
+                <main className={styles.content}>
+                    <div className={styles.filter}>
+                        <div className={styles.header}>
+                            <Typography variant="h5">Filtering:</Typography>
+                        </div>
+                        <div className={styles.divider} />
+                        <div className={styles.form}>
+                        <FormControl>
+                        <Typography id="range-slider" gutterBottom>
+  P
+</Typography>
+
+                            {/* <InputLabel htmlFor="my-input">Email address</InputLabel>
+                            <Input id="my-input" aria-describedby="my-helper-text" /> */}
+                        </FormControl>
+                        </div>
+
+                    </div>
+
+                    <div className={styles.productGrid}>
+                        Right
+                    </div>
+                </main>
+            </>
+        )
     }
 }
+
  
 export default AdminPage;
