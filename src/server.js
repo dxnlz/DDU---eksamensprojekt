30c183d7ec6a711294aa0e5cfcd1793c8b4cb7da
Mathias Gredal <mathiasgredal@icloud.com>
2021-05-18 12:19:13 +0200

    Listen to port env var

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const next = require('next')
 const mqtt = require('./mqtt')
 mqtt.listen();
 
+const port = process.env.PORT? process.env.PORT : 3000;
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -15,8 +16,8 @@ app.prepare().then(() => {
     const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
     handle(req, res, parsedUrl)
-  }).listen(3000, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:'+port)
   })
 })
